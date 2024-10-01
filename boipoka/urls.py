328f49676b58ee7e7813a9a631b643bc46f6ac36
Fahad2240 <fahadish861@gmail.com>
2024-10-02 00:07:35 +0600

    ursl.py

--- boipoka/urls.py
+++ boipoka/urls.py
@@ -23,3 +23,5 @@ urlpatterns = [
     path('',include('boipoka_app.urls')),
     path('admin/', admin.site.urls),
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
+urlpatterns+=static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
