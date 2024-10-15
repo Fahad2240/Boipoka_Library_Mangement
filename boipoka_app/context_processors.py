b53f5c8a1bbf7cf97cc1e3a5454e71da39802d35
Fahad2240 <fahadish861@gmail.com>
2024-10-16 00:09:46 +0600

    notification.htmlcommentings

--- boipoka_app/context_processors.py
+++ boipoka_app/context_processors.py
@@ -4,7 +4,7 @@ from .models import Notifications
 
 def unread_notifications(request):
     if request.user.is_authenticated:
-        temp=Notifications.objects.filter(subscriber=request.user, is_read=False)
+        temp=Notifications.objects.filter(subscriber=request.user, is_read=False)#Fetch all notifications which are unread
         if temp.exists():
             unread_count = temp.count()
         else:
